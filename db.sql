67536609c30c55a818ccc72b70ec0a5d691cb5f4
haruaki11052 <haruaki@gem-project.shop>
2022-10-18 22:13:19 +0900

    add restapi attendances

--- db.sql
+++ db.sql
@@ -2,22 +2,22 @@
 -- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Oct 17, 2022 at 01:30 PM
--- Server version: 5.7.32
--- PHP Version: 7.4.12
+-- ホスト: localhost:8889
+-- 生成日時: 2022 年 10 月 18 日 12:32
+-- サーバのバージョン： 5.7.32
+-- PHP のバージョン: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `kintai`
+-- データベース: `kintai`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `attendances`
+-- テーブルの構造 `attendances`
 --
 
 CREATE TABLE `attendances` (
@@ -26,14 +26,14 @@ CREATE TABLE `attendances` (
   `calendar` date NOT NULL COMMENT '日付',
   `data` json NOT NULL COMMENT '日付,出勤,退勤,休(分),休(区分)',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
-  `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
   `deleted_at` datetime DEFAULT NULL COMMENT '削除日'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='出勤簿';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `paid_periods`
+-- テーブルの構造 `paid_periods`
 --
 
 CREATE TABLE `paid_periods` (
@@ -43,14 +43,14 @@ CREATE TABLE `paid_periods` (
   `end_date` date NOT NULL COMMENT '有効期間終了',
   `number` tinyint(11) NOT NULL DEFAULT '0' COMMENT '日数',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
-  `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
   `deleted_at` datetime DEFAULT NULL COMMENT '削除日'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='有給休暇付与履歴';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- テーブルの構造 `users`
 --
 
 CREATE TABLE `users` (
@@ -59,14 +59,14 @@ CREATE TABLE `users` (
   `login_id` varchar(10) NOT NULL,
   `password` varchar(30) NOT NULL COMMENT 'パスワード',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
-  `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
   `deleted_at` datetime DEFAULT NULL COMMENT '削除日'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vacations`
+-- テーブルの構造 `vacations`
 --
 
 CREATE TABLE `vacations` (
@@ -76,30 +76,30 @@ CREATE TABLE `vacations` (
   `vacation_category_id` smallint(6) NOT NULL COMMENT '休暇区分',
   `remarks` varchar(100) DEFAULT NULL COMMENT '備考',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
-  `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
   `deleted_at` datetime DEFAULT NULL COMMENT '削除日'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vacation_categories`
+-- テーブルの構造 `vacation_categories`
 --
 
 CREATE TABLE `vacation_categories` (
   `id` smallint(11) NOT NULL COMMENT 'id',
   `name` varchar(30) NOT NULL COMMENT '休暇区分名',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '作成日',
-  `updated_at` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
   `deleted_at` datetime DEFAULT NULL COMMENT '削除日'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='休暇区分';
 
 --
--- Indexes for dumped tables
+-- ダンプしたテーブルのインデックス
 --
 
 --
--- Indexes for table `attendances`
+-- テーブルのインデックス `attendances`
 --
 ALTER TABLE `attendances`
   ADD PRIMARY KEY (`id`),
@@ -107,54 +107,72 @@ ALTER TABLE `attendances`
   ADD KEY `user_id_index` (`user_id`) USING BTREE;
 
 --
--- Indexes for table `paid_periods`
+-- テーブルのインデックス `paid_periods`
 --
 ALTER TABLE `paid_periods`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `user_id_index` (`user_id`) USING BTREE;
 
 --
--- Indexes for table `users`
+-- テーブルのインデックス `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `vacations`
+-- テーブルのインデックス `vacations`
 --
 ALTER TABLE `vacations`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vacation_category_id_index` (`vacation_category_id`) USING BTREE,
+  ADD KEY `user_id_index` (`user_id`) USING BTREE;
 
 --
--- Indexes for table `vacation_categories`
+-- テーブルのインデックス `vacation_categories`
 --
 ALTER TABLE `vacation_categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- ダンプしたテーブルの AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `attendances`
+-- テーブルの AUTO_INCREMENT `attendances`
 --
 ALTER TABLE `attendances`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
 
 --
--- AUTO_INCREMENT for table `paid_periods`
+-- テーブルの AUTO_INCREMENT `paid_periods`
 --
 ALTER TABLE `paid_periods`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
 
 --
--- AUTO_INCREMENT for table `users`
+-- テーブルの AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
   MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT COMMENT 'id';
 
 --
--- AUTO_INCREMENT for table `vacations`
+-- テーブルの AUTO_INCREMENT `vacations`
 --
 ALTER TABLE `vacations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
+
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `paid_periods`
+--
+ALTER TABLE `paid_periods`
+  ADD CONSTRAINT `user_id_index` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+
+--
+-- テーブルの制約 `vacations`
+--
+ALTER TABLE `vacations`
+  ADD CONSTRAINT `vacation_category_id_index` FOREIGN KEY (`vacation_category_id`) REFERENCES `vacation_categories` (`id`) ON UPDATE CASCADE;
--- server/index.py
+++ server/index.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask
+from flask import jsonify
 from dotenv import dotenv_values
-
+import mysql.connector
 
 config = dotenv_values(".env")
+
 app = Flask(__name__)
 
 
@@ -11,9 +13,60 @@ def hello():
     return "Hello World"
 
 
-@app.route("/test")
-def test():
-    return "Test.."
+@app.route("/attendances")
+def attendances():
+    # コネクション作成
+    conn = mysql.connector.connect(
+        host=config['DB_HOST'],
+        port=config['DB_PORT'],
+        user=config['DB_USER'],
+        password=config['DB_PASSWORD'],
+        database=config['DB_DATABASE']
+    )
+
+    # 接続状況確認
+    if conn.is_connected():
+        try:
+            # 再接続を行うように設定
+            conn.ping(reconnect=True)
+
+            # カーソル作成
+            cr = conn.cursor()
+
+            # クエリを作成
+            sql = ('''
+                SELECT * FROM attendances
+            ''')
+
+            # selectを実行
+            cr.execute(sql)
+
+            # 全データ取得
+            rows = cr.fetchall()
+
+            # 実行結果
+            response = {
+                'data': rows
+            }
+            return jsonify(response), 200
+
+        except mysql.connector.Error as e:
+            response = {
+                'error': e
+            }
+            return jsonify(response), 400
+
+        finally:
+            # クローズ
+            if cr is not None:
+                cr.close()
+            if conn is not None and conn.is_connected():
+                conn.close()
+    else:
+        response = {
+            'error': 'database connection failed.'
+        }
+        return jsonify(response), 500
 
 
 if __name__ == "__main__":
